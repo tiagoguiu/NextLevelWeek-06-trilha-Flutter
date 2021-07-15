4cd398d517cc7953b7595ed22861240384fbfa96
Tiago Rezende Vilas Boas <tiagoguiu98@hotmail.com>
2021-07-15 12:49:08 -0300

    Finalizando MLKit

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/inser_boleto_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/appColors.dart';
@@ -38,6 +39,7 @@ class AppWidget extends StatelessWidget {
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
         "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto" : (context) => InsertBoletoPage(),
       },
     );
   }
